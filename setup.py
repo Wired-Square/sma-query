8fb64b41a83bae6cba8675db12b909ca34a2b646
Garth Berry <garth@wiredsquare.com>
2022-03-01 12:06:15 +0000

    Move package files into src directory

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/wired-square/sma-query",
-    packages=setuptools.find_packages(),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
